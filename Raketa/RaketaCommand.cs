35db6e3546990d830b9810a3432e11a7ddd3ae52
EddoCoding <codingeprst@gmail.com>
2024-08-13 20:30:10 +0300

    Добавил ракеты

---
+++ Raketa/RaketaCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+namespace Raketa.Commands
+{
+    public class RaketaCommand : ICommand
+    {
+        readonly Action _execeute;
+        readonly Func<object, bool> _canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RaketaCommand(Action execute, Func<object, bool> canExecute = null)
+        {
+            _execeute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object? parameter) => _execeute?.Invoke();
+
+        public static RaketaCommand Launch(Action execute, Func<object, bool> canExecute = null)
+            => new RaketaCommand(execute, canExecute);
+    }
+}
---
+++ Raketa/RaketaTCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+namespace Raketa.Commands
+{
+    public class RaketaTCommand<T> : ICommand
+    {
+        readonly Action<T> _execute;
+        readonly Func<T, bool> _canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RaketaTCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke((T)parameter) ?? true;
+
+        public void Execute(object? parameter) => _execute?.Invoke((T)parameter);
+
+        public static RaketaTCommand<T> Launch(Action<T> execute, Func<T, bool> canExecute = null)
+        => new RaketaTCommand<T>(execute, canExecute);
+    }
+}
