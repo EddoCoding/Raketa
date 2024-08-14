ca3fe5067a4e5a85da1c034f68544deefc86bc94
EddoCoding <codingeprst@gmail.com>
2024-08-14 16:25:45 +0300

    Добавил базовую модель представления

---
+++ Raketa/RaketaViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Raketa
+{
+    public class RaketaViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
+        bool SetValue<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
