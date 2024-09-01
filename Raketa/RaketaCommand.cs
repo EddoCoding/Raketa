5d23a7aead0e9efe65bc1aab8231b76c4dc1393f
EddoCoding <codingeprst@gmail.com>
2024-09-02 02:25:11 +0300

    Переделал некоторые методы асинхроным вызовом

--- Raketa/RaketaCommand.cs
+++ Raketa/RaketaCommand.cs
@@ -6,7 +6,7 @@ namespace Raketa
     {
         readonly Func<Task>? _executeAsync;
         readonly Action? _executeSync;
-        readonly Func<object, bool>? _canExecute;
+        readonly Func<bool>? _canExecute;
 
         public event EventHandler? CanExecuteChanged
         {
@@ -14,19 +14,19 @@ namespace Raketa
             remove => CommandManager.RequerySuggested -= value;
         }
 
-        public RaketaCommand(Func<Task> executeAsync, Func<object, bool> canExecute = null)
+        public RaketaCommand(Func<Task> executeAsync, Func<bool> canExecute = null)
         {
             _executeAsync = executeAsync ?? throw new ArgumentNullException(nameof(executeAsync));
             _canExecute = canExecute;
         }
 
-        public RaketaCommand(Action executeSync, Func<object, bool> canExecute = null)
+        public RaketaCommand(Action executeSync, Func<bool> canExecute = null)
         {
             _executeSync = executeSync ?? throw new ArgumentNullException(nameof(executeSync));
             _canExecute = canExecute;
         }
 
-        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
 
         public async void Execute(object? parameter)
         {
@@ -42,5 +42,11 @@ namespace Raketa
                 }
             }
         }
+
+        public static RaketaCommand Launch(Func<Task> execute, Func<bool> canExecute = null) =>
+            new RaketaCommand(execute, canExecute);
+
+        public static RaketaCommand Launch(Action execute, Func<bool> canExecute = null) =>
+            new RaketaCommand(execute, canExecute);
     }
 }
